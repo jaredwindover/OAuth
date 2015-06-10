ab26882a805cc786d08d43f513488389daeaa4c4
Jared Windover <jaredwindover@gmail.com>
2015-06-09 21:08:29 -0400

    Next stuff

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-var express = require('express');
+var express    = require('express');
+var mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
+var Beer = require('./models/beer');
+
+mongoose.connect('mongodb://BeerLocker:1o1u2a3m5d@ds031721.mongolab.com:31721/heroku_app37684533');
 
 var app = express();
 
@@ -9,6 +14,27 @@ var router = express.Router();
 router.get('/', function(req,res) {
     res.json({message: 'You are running dangerously low on beer!'});
 });
+
+var beersRoute = router.route('/beers');
+
+beersRoute.post(function(req, res) {
+    var beer = new Beer();
+
+    beer.name = req.body.name;
+    beer.type = req.body.type;
+    beer.quantity = req.body.quantity;
+
+    beer.save(function(err) {
+	if (err) {
+	    res.send(err);
+	}
+	res.json({ message: 'Beer added to the locker!', data: beer});
+    });
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.use('/api',router);
 
