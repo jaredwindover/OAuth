0f22cc9421c3eb2925db4f60ff22381bf35694da
Jared Windover <jaredwindover@gmail.com>
2015-06-11 09:24:13 -0400

    Adding basic authentication

--- models/user.js
+++ models/user.js
@@ -33,4 +33,13 @@ UserSchema.pre('save', function(callback) {
     
 });
 
+UserSchema.methods.verifyPassword = function(password, cb) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+	if (err) {
+	    return cb(err);
+	};
+	cb(null, isMatch);
+    });
+};
+
 module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express    = require('express');
 var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
+var passport   = require('passport');
 var beerController = require('./controllers/beer');
 var userController = require('./controllers/user');
+var authController = require('./controllers/auth');
 
 mongoose.connect('mongodb://BeerLocker:1o1u2a3m5d@ds031721.mongolab.com:31721/heroku_app37684533');
 
@@ -13,23 +15,25 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 
 router.route('/beers')
-    .post(beerController.postBeers)
-    .get(beerController.getBeers);
+    .post(authController.isAuthenticated, beerController.postBeers)
+    .get(authController.isAuthenticated, beerController.getBeers);
 
 router.route('/beers/:beer_id')
-    .get(beerController.getBeer)
-    .put(beerController.putBeer)
-    .delete(beerController.deleteBeer);
+    .get(authController.isAuthenticated, beerController.getBeer)
+    .put(authController.isAuthenticated, beerController.putBeer)
+    .delete(authController.isAuthenticated, beerController.deleteBeer);
 
 router.route('/users')
     .post(userController.postUsers)
-    .get(userController.getUsers);
+    .get(authController.isAuthenticated, userController.getUsers);
 
 app.use(bodyParser.urlencoded({
     extended: true
-}))
+}));
 
 app.use('/api',router);
+
+app.use(passport.initialize());
 
 app.listen(port);
 
