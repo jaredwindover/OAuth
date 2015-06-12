3c9ce6255624fcf5488e2ebe2824775e58ca65b4
Jared Windover <jaredwindover@gmail.com>
2015-06-12 19:04:51 -0400

    allowing oauth2

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('../models/user');
+var Token = require('../models/token');
 
 passport.use(new BasicStrategy(
     function(username, password, callback) {
@@ -16,6 +17,44 @@ passport.use(new BasicStrategy(
     }
 ));
 
+passport.use('client-basic', new BasicStrategy(
+    function(username, password, callback) {
+	client.findOne({id:username}, function(err, client) {
+	    if (err) {return callback(err);}
+
+	    if (!client || client.secret !== password) {
+		return callback(null, false);
+	    }
+
+	    return callback(null, client);
+	});
+    }
+));
+
+passport.use(new BearerStrategy(
+    function(accessToken, callback){
+	Token.findOne({value: accessToken}, function(err, token){
+	    if (err) {return callback(err);}
+
+	    if (!token) {
+		return callback(null, false);
+	    }
+
+	    User.findOne({_id: token.userId}, function(err, user) {
+		if (err) {return callback(err);}
+		if (!user) {return callback(null, false);}
+		callback(null, user, {scope: '*'});
+	    });
+	});
+    }
+));
+
 exports.isAuthenticated = passport.authenticate(
-    'basic',
+    ['basic', 'bearer'],
     {session: false});
+
+exports.isClientAuthenticated = passport.authenticate(
+    'client-basic',
+    {session: false});
+
+exports.isBearerAuthenticated = passport.authenticate('bearer', {session: false});
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 var express    = require('express');
+var session    = require('express-session');
 var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
 var passport   = require('passport');
-var beerController = require('./controllers/beer');
-var userController = require('./controllers/user');
-var authController = require('./controllers/auth');
+var ejs        = require('ejs');
+var beerController   = require('./controllers/beer');
+var userController   = require('./controllers/user');
+var authController   = require('./controllers/auth');
 var clientController = require('./controllers/client');
+var oauth2Controller = require('./controllers/oauth2');
 
 mongoose.connect('mongodb://BeerLocker:1o1u2a3m5d@ds031721.mongolab.com:31721/heroku_app37684533');
 
 var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(session({
+    secret: 'Super Secret Session Key',
+    saveUninitialized: true,
+    resave: true
+}));
 
 var port = process.env.PORT || 3000;
 
@@ -32,9 +47,12 @@ router.route('/clients')
     .post(authController.isAuthenticated, clientController.postClients)
     .get(authController.isAuthenticated, clientController.getClients)
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+router.route('/oauth2/authorize')
+    .get(authController.isAuthenticated, oauth2Controller.authorization)
+    .post(authController.isAuthenticated, oauth2Controller.decision);
+
+router.route('/oauth2/token')
+    .post(authController.isClientAuthenticated, oauth2Controller.token);
 
 app.use(passport.initialize());
 
