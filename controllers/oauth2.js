b85094853ce87dbc68621f47a62269a04a1307f6
Jared Windover <jaredwindover@gmail.com>
2015-06-12 19:23:17 -0400

    typo

--- controllers/oauth2.js
+++ controllers/oauth2.js
@@ -2,7 +2,7 @@ var oauth2orize = require('oauth2orize');
 var User        = require('../models/user');
 var Client      = require('../models/client');
 var Token       = require('../models/token');
-var Code        = require('..models/code');
+var Code        = require('../models/code');
 
 var server = oauth2orize.createServer();
 
